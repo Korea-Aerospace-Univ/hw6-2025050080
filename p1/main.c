904a52e6b292355774a9f338c4418fe8f6e81b56
2025050080 <park68447@gmail.com>
2026-05-27 16:05:35 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,31 +3,31 @@
 int main(void)
 {
     char x;
-    int c2=1;
-    int c1=0;
-    char arr[10]={};
-    char *p=nullptr;
+    int c2 = 1;
+    int c1 = 0;
+    char arr[10] = {};
+    char *p = nullptr;
     
-    for(p=arr;p<arr+10;p++){
+    for(p = arr; p < arr + 10; p++){
         scanf(" %c", p);
     }
 
-    x=*p;
+    x = *p;
       
-    for(p=arr;p<arr+10;p++){
-        for(char*q=arr;q<arr+10;q++){
-            if(*p==*q){
+    for(p = arr; p < arr + 10; p++){
+        for(char *q = arr; q < arr + 10; q++){
+            if(*p == *q){
                 c1++;
                 
-                if(c1>c2){
-                   c2=c1;
-                   x=*p;
+                if(c1 > c2){
+                   c2 = c1;
+                   x = *p;
                 }
             }
         }
-        c1=0;
+        c1 = 0;
     } 
-    printf("%c""%d", x, c2);
+    printf("%c %d", x, c2);
 
     return 0;
 }
